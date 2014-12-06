e34448040e7c9b36aff2cb6c091e6fe1a3d73fc6
jaimereycasado <jaimereycasado@gmail.com>
2014-12-06 23:40:08 +0100

    first distribution as jimereycasado.com/prototype new route: articulos

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.bootstrap'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -26,6 +27,10 @@ angular
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
+      })
+      .when('/articulos', {
+        templateUrl: 'views/articulos.html',
+        controller: 'ArticulosCtrl'
       })
       .otherwise({
         redirectTo: '/'
